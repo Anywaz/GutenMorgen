43b5bf2ca0536770a3d3a666551c7bd76675ca40
scheidha <felix.scheidhammer@tum.de>
2018-06-16 15:04:56 +0200

    removed second water

--- Guten Morgen/Assets/Scripts/Juice.cs
+++ Guten Morgen/Assets/Scripts/Juice.cs
@@ -3,21 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Juice : WaterTap {
-	public ParticleSystem water;
 	//position of the juice at the beginning
 	private Transform startPos;
 
 	public void onClick() {
 		//start waterflow
 		base.onClick();
-		if(transform.parent == null) {
-			//hold in hand:
-			//store old pos to reset it later:
-		}
-		else {
-			transform.parent = null;
-			transform.position = startPos.position;
-			transform.rotation = startPos.rotation;
-		}
+		transform.SetParent(GameObject.FindWithTag("Player").transform);
+
 	}
 }
