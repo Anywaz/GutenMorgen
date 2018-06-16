d419c0f86477438e5dec2f6cac6f759fc677066a
Lokificent <sonja.stefani@yahoo.de>
2018-06-16 11:22:30 +0200

    added checkList
    *Simple checklist activation and collection, nothing major

---
+++ Guten Morgen/Assets/Scripts/Checklist.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checklist : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnEnable()
+    {
+        Debug.Log("This is in enable, we are enabled!");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("This is in disable, we were disabled :(");
+    }
+}
--- Guten Morgen/Assets/Scripts/OurRayCaster.cs
+++ Guten Morgen/Assets/Scripts/OurRayCaster.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class OurRayCaster : MonoBehaviour {
 
 	public float raycastRange;
+    public GameObject checklist;
+    private bool isChecklistActive;
+    private bool checklistAcquired;
 	// Use this for initialization
 	void Start () {
-		
+        isChecklistActive = false;
+        checklistAcquired = false;
 	}
 	
 	// Update is called once per frame
@@ -19,13 +23,38 @@ public class OurRayCaster : MonoBehaviour {
 			bool didHit = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, raycastRange);
 			Debug.Log("i hit:" + didHit);
 			if(didHit) {
+                Debug.Log(hit.transform.tag);
 				Clickable clicked = hit.transform.gameObject.GetComponent<Clickable>();
+                if (hit.transform.tag.Equals("checklist"))
+                {
+                    Debug.Log("hit is checklist");
+                    checklistAcquired = true;
+                    hit.transform.position = this.transform.position;
+                    hit.transform.SetParent(this.transform);
+                    //hier animation der checklist stoppen?
+                }
 				if(clicked != null) {
 					//we hit a clickable!
 					clicked.onClick();
 					Debug.Log("onClick");
 				}
 			}
+        }
+        if (Input.GetKeyDown(KeyCode.H) && checklistAcquired)
+        {
+            if (isChecklistActive)
+            {
+                //deactivate
+                isChecklistActive = false;
+                checklist.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("pushed H key.");
+                Debug.Log("isChecklistActive?: " + isChecklistActive);
+                checklist.SetActive(true);
+                isChecklistActive = true;
+            }
         }
 	}
 }
