2bf81d1839c66c06892125690420c122f0dc0641
Lokificent <sonja.stefani@yahoo.de>
2018-06-16 14:46:37 +0200

    added checkListLogic
    * enabling the checklist will make random list entries appear
    * list entries save whether they've been done or not
    * this is not yet display !

--- Guten Morgen/Assets/Scripts/checkListLogic.cs
+++ Guten Morgen/Assets/Scripts/checkListLogic.cs
@@ -2,16 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class checkListLogic : MonoBehaviour {
+public class checkListLogic : MonoBehaviour
+{
     public GameObject checkmark;
+    private List<task> tasks;
+    private string[] taskDescriptions = { "Brush teeth", "Eat an apple", "This is an example", "What else is there really?", "Drink some sweet OJ", "How 'bout sum coffee", "Crumpets sound nice?", "*incoherent singing*" };
+    public List<GameObject> listpoints;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public struct task
+    {
+        public bool isDone;
+        public string description;
+
+        public task(bool done, string desc)
+        {
+            isDone = done;
+            description = desc;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (GameObject o in listpoints)
+        {
+            o.GetComponent<TextMesh>().text = "";
+        }
+        listpoints[0].GetComponent<TextMesh>().text = "Read Me!";
+        instantiateTasks();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("reached OnEnable with isCaught");
+        task[] chosen = findTasks();
+        for (int i = 0; i < 5; i++)
+        {
+            listpoints[i].GetComponent<TextMesh>().text = chosen[i].description;
+        }
+    }
+
+    private task[] findTasks()
+    {
+        task[] chosenTasks = new task[5];
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log("This is i: " + i + ".");
+            int next = (int)Random.Range(0.0f, (float)taskDescriptions.Length);
+            Debug.Log("Next is: " + next + ".");
+            chosenTasks[i] = tasks[next];
+        }
+        return chosenTasks;
+    }
+
+    private void instantiateTasks()
+    {
+        tasks = new List<task>();
+        for (int i = 0; i < taskDescriptions.Length; i++)
+        {
+            Debug.Log("Instantiated task " + i + " with desc " + taskDescriptions[i]);
+            tasks.Add(new task(false, taskDescriptions[i]));
+        }
+        foreach (task t in tasks)
+        {
+            Debug.Log("Desc:" + t.description + ", isDone = " + t.isDone);
+        }
+    }
 }
